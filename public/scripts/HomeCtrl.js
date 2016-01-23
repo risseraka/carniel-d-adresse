fee8840439cf5cd83e03d92524658e50ea6837b1
jmpp <jmclery@gmail.com>
2016-01-23 13:50:58 +0100

    Ajout de l'interface

--- public/scripts/HomeCtrl.js
+++ public/scripts/HomeCtrl.js
@@ -1,10 +1,24 @@
 (function(app) {
   'use strict';
 
-  app.controller('HomeCtrl', function() {
+  app.controller('HomeCtrl', function($rootScope, $scope, fileUpload) {
     var home = this;
 
+    home.error = false;
+    home.carnet = $rootScope.carnet;
 
+    home.uploadFile = function() {
+      fileUpload.uploadFileToUrl($scope.file, '/', fileUploaded);
+    };
+
+    home.removeContact = function(id) {
+      console.log('removing #'+id)
+    }
+
+    function fileUploaded(err, response) {
+      if (err)
+        return home.error = err;
+    }
   });
 
 })(angular.module('CarnielApp'));
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,6 @@
 (function(app) {
   'use strict';
 
-  console.log('here')
-
   app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
@@ -23,5 +21,51 @@
         }
       })
   });
+
+  // Lancement de l'app
+  app.run(function($rootScope, $http) {
+
+    $rootScope.carnet = [];
+
+    $http.get('/api/contacts')
+      .then(function(response) {
+        $rootScope.carnet = response.data.contacts;
+      });
+  });
+
+  // https://uncorkedstudios.com/blog/multipartformdata-file-upload-with-angularjs
+  app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+
+        element.bind('change', function(){
+          scope.$apply(function(){
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      }
+    };
+  }])
+  .service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl, cb){
+      var fd = new FormData();
+      fd.append('file', file);
+      $http.post(uploadUrl, fd, {
+        transformRequest: angular.identity,
+        headers: {'Content-Type': undefined}
+      })
+      .success(function(data){
+        console.log(data)
+        cb(null, data);
+      })
+      .error(function(err){
+        console.log('oups')
+        cb(err);
+      });
+    }
+  }]);
 
 })(angular.module('CarnielApp', ['ui.router']));
